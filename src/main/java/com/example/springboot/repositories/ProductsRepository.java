cc417bbf6cefb2b6ee2052e58fb9c3869480b468
ViniciusSilvaDeOliveira <tiviniciusoliveira@gmail.com>
2023-07-02 11:42:25 -0300

    Atualização Projeto

--- src/main/java/com/example/springboot/repositories/ProductsRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductsRepository.java
@@ -1,4 +1,10 @@
 package com.example.springboot.repositories;
 
-public interface ProductsRepository {
+import com.example.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface ProductsRepository extends JpaRepository<ProductModel, UUID> {
 }
